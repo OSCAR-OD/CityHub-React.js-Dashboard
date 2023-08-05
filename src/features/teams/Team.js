921e2297e6ee1b6876ca5cb734678ed2bc3ad687
OSCAR DEB <20-42888-1@Student.aiub.edu>
2023-08-06 01:13:28 +0600

    first1

--- src/features/teams/Team.js
+++ src/features/teams/Team.js
@@ -13,24 +13,25 @@ const Team = ({ teamId }) => {
 
     if (team) {
         const created = new Date(team.createdAt).toLocaleString('en-US', { day: 'numeric', month: 'long' })
-
         const updated = new Date(team.updatedAt).toLocaleString('en-US', { day: 'numeric', month: 'long' })
-
         const handleEdit = () => navigate(`/dash/teams/${teamId}`)
+
+        const teamEmailsString = team.emails.toString().replaceAll(',', ', ')
 
         return (
             <tr className="table__row">
                 <td className="table__cell team__status">
                     {team.completed
                         ? <span className="team__status--completed">Completed</span>
-                        : <span className="team__status--open">Open</span>
+                        : <span className="team__status--open">Active</span>
                     }
                 </td>
                 <td className="table__cell team__created">{created}</td>
                 <td className="table__cell team__updated">{updated}</td>
                 <td className="table__cell team__title">{team.title}</td>
                 <td className="table__cell team__userEmail">
-                   {team.emails}
+                   {/* {team.emails} */}
+                   {teamEmailsString}
                 </td>
                 {/* ok for single , but not for multiple emails, 
                 //in team.name only 1 name is shows
