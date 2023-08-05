f9cc5f43dd17f854cd494b78e286c9b674dd1cf5
OSCAR DEB <20-42888-1@Student.aiub.edu>
2023-08-05 11:12:43 +0600

    Deployed

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://cityhub-api.onrender.com',
+    baseUrl: 'https://cityhub-api.vercel.app',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
